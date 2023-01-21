b9c81f974a7b1a21820e2082189c7518c9ca73c4
Vitaliy Potapov <noginsk@rambler.ru>
2023-01-21 11:55:40 +0400

    use new eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+const error = "error";
+
+export default [
+  "eslint:recommended",
+  {
+    files: [ "**/*.ts" ],
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.node,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "no-console": error,
+      "complexity": [error, { max: 5 }],
+      "max-depth": [error, { max: 2 }],
+      "max-nested-callbacks": [error, { max: 2 }],
+      "max-params": [error, { max: 3 }],
+      "max-statements": [error, { max: 12 }, { ignoreTopLevelFunctions: false }],
+      "max-len": [error, { code: 120, ignoreUrls: true }],
+      "max-lines": [error, { max: 150, skipComments: true, skipBlankLines: true }],
+      "semi": [error, "always"],
+      "no-multiple-empty-lines": [error, { max: 1 }],
+      "space-before-function-paren": [error, { anonymous: "always", named: "never", asyncArrow: "always" }],
+      "@typescript-eslint/no-unused-vars": error,
+    }
+  },
+  {
+    files: [ "test/**/*.ts" ],
+    languageOptions: {
+      globals: globals.mocha,
+    },
+  },
+];
